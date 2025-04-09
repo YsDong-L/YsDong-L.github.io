04d2f25b7bcd014e993dd07bdb933829f2e876c2
YsDong-L <19862639811@163.com>
2025-04-09 22:59:23 +0800

    Site updated: 2025-04-09 22:59:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/04/hello-world1/","2025/04/09/day 1 -二分法详解/","2025/04/09/day 1/","2025/04/07/构建单向链表/"];function toRandomPost(){
+var posts=["2025/04/09/day 1 -二分法详解/","2025/04/09/day 1/","2025/04/04/hello-world1/","2025/04/07/构建单向链表/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
